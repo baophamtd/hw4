eebc00406a8a44f549d85988eda61ea90f4cb749
Cassidy <tarng.cassidy@gmail.com>
2016-11-13 18:34:19 -0800

    Improved error message

--- src/controllers/LandingPageController.php
+++ src/controllers/LandingPageController.php
@@ -13,35 +13,6 @@ class LandingPageController extends Controller{
     
     function processRequest() {
 			$data = [];
-			$data['title'] = "";
-			$data['content'] = "";
-		 
-			if (isset($_REQUEST['title']) && isset($_REQUEST['content'])){
-				$splitContent = split("\n", $_REQUEST['content']);
-				// Checks if there are more than 50 lines
-				if (count($splitContent) > 50){
-					$data['textAreaError'] = true;
-					$data['title'] = $_REQUEST['title'];
-					$data['content'] = $_REQUEST['content'];
-				}
-				foreach ($splitContent as $singleLine){
-					// Checks if each line has more than 80 characters
-					if (strlen($singleLine) > 80){
-						$data['textAreaError'] = true;
-						$data['title'] = $_REQUEST['title'];
-						$data['content'] = $_REQUEST['content'];
-						break;
-					}
-					// Checks if each line has 2 commas
-					else if (count(split(",", $singleLine)) != 3){
-						$data['textAreaError'] = true;
-						$data['title'] = $_REQUEST['title'];
-						$data['content'] = $_REQUEST['content'];
-						break;
-					}
-				}
-			}
-
             $view = new B\views\LandingPageView();
             $view->render($data);
             
--- src/views/LandingPageView.php
+++ src/views/LandingPageView.php
@@ -16,23 +16,25 @@ class LandingPageView extends View{
         <head>
 		
 		<script type="text/javascript">
-		function setMessage(){
-				document.write("Format was not followed. Please try again.");
-				setTimeout(function(){document.location.href="index.php"},3000);
-			}
+	
+	// Sets the error message
+	function setMessage(){
+		document.getElementById("error").innerHTML = "Format was not followed. Please try again.";
+		setTimeout(function(){document.location.href="index.php"},3000);
+	}
 			
 	// Checks from the client side
 	function textareaCheck(){
 		var content = document.forms["myForm"]["content"].value;
 		var splitContent = content.split("\n");
 		// Checks if there's more than 50 lines
-		if (splitContent.length > 5){
+		if (splitContent.length > 50){
 			setMessage();
 			return false;
 		}
 		// Checks if there's more than 80 characters per line
 		for (var i = 0; i < splitContent.length; i++){
-			if (splitContent[i].length > 10){
+			if (splitContent[i].length > 80){
 				setMessage();
 				return false;
 			}
@@ -57,14 +59,15 @@ class LandingPageView extends View{
             <br>
 			<form name="myForm" action='index.php' method='get' onsubmit="return textareaCheck()">
             <label for = "title">Chart Title: </label>
-            <input type = "text" id = "title" name = "title" value = <?=$data['title']?>>
+            <input type = "text" id = "title" name = "title">
             </br>
             <br>
             <textarea name = "content" id = "content" rows="10" cols="50" placeholder="'Text Label','Coordinate 1','Coordinate 2'
-One value per line, up to 50 lines of at most 80 characters"><?=$data['content']?></textarea>
+One value per line, up to 50 lines of at most 80 characters"></textarea>
             </br>
             <input type="submit" value="Share"/>
 			</form>
+			<p id="error"></p>
             </div>
         </body>
     </html>
