ce9a62fd9a598630c08262edd3c89da37d0531b1
Kareem Darkazanli <kdark93@gmail.com>
2016-11-18 13:17:08 -0800

    Nothing

--- index.php
+++ index.php
@@ -11,15 +11,25 @@ define(NS_BASE . "NS_CONTROLLERS", "threemuskateers\\hw4\\controllers\\");
 
 $controller_name = NS_CONTROLLERS . "LandingPageController";
 
+$array = array();
+$data = array();
 // Checks from the serverside
 if (isset($_REQUEST['title']) && isset($_REQUEST['content'])){
 	$splitContent = explode("\n", $_REQUEST['content']);
 	// Checks if there are more than 50 lines
 	if (count($splitContent) <= 50){
 		foreach ($splitContent as $singleLine){
-			// Checks if each line has more than 80 characters
+			// Checks if each line has less than 80 characters
 			if (strlen($singleLine) <= 80){
 				$splitLine = explode(",", $singleLine);
+				$index = 1;
+				while($index < count($splitLine)){
+				    array_push($data,$splitLine[$index]);
+				    $index = $index + 1;
+				}
+				$array[$splitLine[0]] = $data;
+			    print_r($array);
+
 				// Checks if each line has 2 commas
 				if (count($splitLine) == 3){
 					if (!empty(reset($splitLine))){
