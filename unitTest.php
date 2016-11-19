880f643cb740469ebc0e6aabd76dd0e37675e76f
Cassidy <tarng.cassidy@gmail.com>
2016-11-19 15:53:09 -0800

    Finished unit testing

---
+++ unitTest.php
@@ -0,0 +1,35 @@
+<?php
+use threemuskateers\hw4\controllers;
+require_once(dirname(__FILE__) . '/simpletest/autorun.php');
+require_once('src/controllers/LandingPageController.php');
+
+class TestOfLogging extends UnitTestCase {
+    function __construct() {
+        parent::__construct('LandingPageController test');
+    }
+
+    function testFirstLogMessagesCreatesFileIfNonexistent() {
+        @unlink(dirname(__FILE__) . 'temp/test.log');
+        $log = new threemuskateers\hw4\controllers\LandingPageController(dirname(__FILE__) . 'temp/test.log');
+		
+		// Test number of items in tuple and tuple properties
+		$this->assertEqual($log->didCheckPass("test1","a,a,a"), true);
+		$this->assertEqual($log->didCheckPass("test2","a"), false);
+		$this->assertEqual($log->didCheckPass("test3","a,a,a,a,a,a"), true);
+		$this->assertEqual($log->didCheckPass("test4","a,a,a,a,a,a,a"), false);
+		$this->assertEqual($log->didCheckPass("test5",",a,a"), false);
+		
+		// Test when there are 50 lines
+		$this->assertEqual($log->didCheckPass("test6","a,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a"), true);
+		
+		// Test when there are more than 50 lines
+		$this->assertEqual($log->didCheckPass("test7","a,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a\na,a,a"), false);
+		
+		// Test when there are 80 characters in a line
+		$this->assertEqual($log->didCheckPass("test8","aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa,aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"), true);
+		
+		// Test when there are more than 80 characters in a line
+		$this->assertEqual($log->didCheckPass("test9","aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa,aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"), false);
+    }
+}
+?>
